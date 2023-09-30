19ae26f89dabd1346b5324d5a7d82621df114c87
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2023-09-29 22:38:11 -0400

    Declaration
    Début du projet S5

---
+++ Project/Declaration.m
@@ -0,0 +1,62 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% @file: Declaration.m
+% @Date: 2023/09/29
+%
+% @Name: - Shawn Miller (mils2203)
+%                                      
+%
+% @Brief: Décalaration des Variables utilisé dans le projet
+%   
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% ∗ _𝑚𝑒𝑠 : réfère à des variables mesurées
+% ∗ _𝑑𝑒𝑠 : réfère aux variables désirées ou commandées
+% ∗ _𝑖𝑛𝑖 : réfère aux variables initiales
+% ∗ _𝑓𝑖𝑛 : réfère aux variables finales
+% ∗ _𝑒𝑞 : réfère aux variables à l’équilibre
+
+masseS = 0; % Masse de la sphère
+masseP = 0; % Masse de la plaque
+
+inertiePx = 0; % Inertie de la plaque autour de l'axe x
+inertiePy = 0; % Inertie de la plaque autour de l'axe y
+
+XK = 0; % Position inertielle des éléments fixes K (K = A, B, C, D, E, F)
+YK = 0; 
+ZK = 0; 
+
+Px = 0; % Position de la sphère dans le repère inertiel
+Py = 0;
+Pz = 0;
+
+Vx = 0; % Vitesse de la sphère dans le repère inertiel
+Vy = 0;
+Vz = 0;
+
+Ax = 0; % Angle de rotation de la plaque phi autoir de l'axe Ix et theta autour de l'axe Iy
+Ay = 0;
+
+Wx = 0; % Vitesse angulaire de la plaque phi autoir de l'axe Ix et theta autour de l'axe Iy
+Wy = 0;
+
+FA = 0; % Forces appliquée par les actionneurs sur la plaque (positif vers le bas)
+FB = 0;
+FC = 0;
+
+MA = 0; % Couple appliquée par les actionneurs sur la plaque
+MB = 0;
+MC = 0;
+
+VA = 0; % Tension électrique appliquée aux actionneurs
+VB = 0;
+VC = 0;
+
+IA = 0; % Courant électrique dans les actionneurs
+IB = 0;
+IC = 0;
+
+
+
+
+
---
+++ Project/main.m
@@ -0,0 +1,23 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% @file: main.m
+% @Date: 2023/09/29
+%
+% @Name: - Shawn Miller (mils2203)
+%        -      
+%        -      
+%        -      
+%        -      
+%        -      
+%        -      
+%
+% @Brief: main projet
+%   
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc 
+close all
+clear all
+
+Declaration; % ajoute le fichier Declaration.m 
+
+
