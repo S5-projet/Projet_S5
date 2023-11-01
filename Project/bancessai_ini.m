8f31d6403ed142d4866678af1ef4c90451357a18
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2023-11-01 15:19:09 -0400

    ajustement equation

--- Project/bancessai_ini.m
+++ Project/bancessai_ini.m
@@ -15,22 +15,13 @@
 %
 %%
 
-
+Fek_Fs
 courant_voltage
 matrice_L
 porjet_1
-Fek_Fs
 
 
-ae0 = Fe_coeff(1);
-ae1 = Fe_coeff(2);
-ae2 = Fe_coeff(3);
-ae3 = Fe_coeff(4);
-as0 = Fs_coeff(1);
-as1 = Fs_coeff(2);
-as2 = Fs_coeff(3);
-as3 = Fs_coeff(4);
-be1 = 13.029359254409743;
+
 
 
 
